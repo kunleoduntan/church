e6b8b72d6e194bd88b6ce04d832e45a67d7475ba
kunleoduntan <kunleoduntan@gmail.com>
2025-01-22 07:39:53 +0100

    v2
    v2

---
+++ church/church/doctype/church_department/test_church_department.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, kunle and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestChurchDepartment(FrappeTestCase):
+	pass
---
+++ church/church/doctype/investment_option/investment_option.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, kunle and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Investment Option', {
+	// refresh: function(frm) {
+
+	// }
+});
